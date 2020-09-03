3f723782f3545c0378af5a49a5053f049efaa875
Dominik <32032778+dommyrock@users.noreply.github.com>
2020-09-03 19:37:32 +0200

    side nav css refactor

--- src/components/card/JobExtrasCard.js
+++ src/components/card/JobExtrasCard.js
@@ -11,28 +11,68 @@ import GraphIcon from "./icons/GraphIcon";
 const JobExtrasCard = () => {
   return (
     <div className={extras_div} style={{ fontWeight: "700px" }}>
-      <h4>
-        <CalculatorIcon width={18} height={18} />
-        Salary calculator
-      </h4>
-      <h4>
-        <PostIcon width={18} height={18} />
-        Original post
-      </h4>
-      <h4>
-        <RangeIcon width={18} height={18} />
-        Salary ranges
-      </h4>
-      <h4>
-        <ListIcon width={21} height={21} />
-        Similar jobs
-      </h4>
-      <h4>
-        <GraphIcon width={18} height={18} />
-        Cost of living
-      </h4>
+      <div>
+        <h4>
+          <CalculatorIcon width={18} height={18} />
+          Salary calculator
+        </h4>
+      </div>
+      <div>
+        <h4>
+          <PostIcon width={18} height={18} />
+          Original post
+        </h4>
+      </div>
+
+      <div>
+        <h4>
+          <RangeIcon width={18} height={18} />
+          Salary ranges
+        </h4>
+      </div>
+
+      <div>
+        <h4>
+          <ListIcon width={20} height={20} />
+          Similar jobs
+        </h4>
+      </div>
+
+      <div>
+        <h4>
+          <GraphIcon width={18} height={18} />
+          Cost of living
+        </h4>
+      </div>
     </div>
   );
 };
 
 export default JobExtrasCard;
+
+/**
+ * NEUMORPHIC CSS TODO
+
+  1
+  #8dadc9
+  or
+  #8698a7
+
+  border-radius: 28px;
+  background: #8dadc9;
+  box-shadow:  27px 27px 55px #728ca3,
+              -27px -27px 55px #a8ceef;
+
+
+  narrow/tighter
+  #88bce7
+  or
+  #87bce8
+  or
+  #d8e4ee
+
+  border-radius: 28px;
+  background: #88bce7;
+  box-shadow:  7px 7px 14px #74a0c4,
+              -7px -7px 14px #9cd8ff;
+ */
