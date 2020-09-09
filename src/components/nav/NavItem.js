e13faef4337df8bbe00bc1611596aa9400b228f0
Dominik <32032778+dommyrock@users.noreply.github.com>
2020-09-09 20:05:57 +0200

    updated navigation links , analytics icon bugfix

--- src/components/nav/NavItem.js
+++ src/components/nav/NavItem.js
@@ -1,11 +1,17 @@
 import React from "react";
 import useComponentVisible from "../helpers/hooks/useComponentVisible";
+// import Link from 'next/link'
 
 const NavItem = (props) => {
   const { ref, isComponentVisible, setIsComponentVisible } = useComponentVisible(false);
   return (
     <li ref={ref} className="nav-item">
-      <a href="#" className="icon-button" onClick={() => setIsComponentVisible(!isComponentVisible)}>
+      <a
+        href={props.link}
+        className="icon-button"
+        onClick={() => setIsComponentVisible(!isComponentVisible)}
+        title={props.title}
+      >
         {props.icon}
       </a>
       {isComponentVisible && props.children}
--- src/components/nav/NavMain.js
+++ src/components/nav/NavMain.js
@@ -3,6 +3,7 @@ import Navbar from "./Navbar";
 import NavItem from "./NavItem";
 import DropdownMenu from "./DropdownMenu";
 import SearchIcon from "../search/SearchIcon";
+import Link from "next/link";
 //Bootstrap icons: https://icons.getbootstrap.com/
 //Ionic icons: https://ionicons.com/
 //TODO: install npm csstransition
@@ -12,6 +13,8 @@ const NavMain = () => {
     <Navbar>
       <SearchIcon width={25} height={25} />
       <NavItem
+        link="#"
+        title="Menu"
         icon={
           <svg
             width="1em"
@@ -30,7 +33,25 @@ const NavMain = () => {
       >
         <DropdownMenu></DropdownMenu>
       </NavItem>
-      <NavItem icon={<i className="fa fa-bar-chart" aria-hidden="true"></i>} />
+      <NavItem
+        link="/analytics"
+        title="Analytics"
+        icon={
+          <svg
+            width="1em"
+            height="1em"
+            viewBox="0 0 16 16"
+            className="bi bi-graph-up"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M0 0h1v15h15v1H0V0zm10 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V4.9l-3.613 4.417a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61L13.445 4H10.5a.5.5 0 0 1-.5-.5z"
+            />
+          </svg>
+        }
+      ></NavItem>
     </Navbar>
   );
 };
